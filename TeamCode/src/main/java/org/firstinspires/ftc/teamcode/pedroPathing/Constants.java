5a0e852f97bd2d5f62e01ca858eef0a97f9ae197
Sharon <147839877+Shr00mp@users.noreply.github.com>
2025-11-25 23:11:27 +0000

    Tuned robot

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/Constants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/Constants.java
@@ -3,17 +3,51 @@ package org.firstinspires.ftc.teamcode.pedroPathing;
 import com.pedropathing.follower.Follower;
 import com.pedropathing.follower.FollowerConstants;
 import com.pedropathing.ftc.FollowerBuilder;
+import com.pedropathing.ftc.drivetrains.MecanumConstants;
+import com.pedropathing.ftc.localization.Encoder;
+import com.pedropathing.ftc.localization.constants.DriveEncoderConstants;
 import com.pedropathing.paths.PathConstraints;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class Constants {
-    public static FollowerConstants followerConstants = new FollowerConstants();
+    private static final Logger log = LoggerFactory.getLogger(Constants.class);
+    public static FollowerConstants followerConstants = new FollowerConstants()
+            .mass(3);
 
     public static PathConstraints pathConstraints = new PathConstraints(0.99, 100, 1, 1);
 
     public static Follower createFollower(HardwareMap hardwareMap) {
         return new FollowerBuilder(followerConstants, hardwareMap)
                 .pathConstraints(pathConstraints)
+                .mecanumDrivetrain(driveConstants)
+                .driveEncoderLocalizer(localizerConstants)
                 .build();
     }
+
+    public static MecanumConstants driveConstants = new MecanumConstants()
+            .maxPower(1)
+            .rightFrontMotorName("rf")
+            .rightRearMotorName("rr")
+            .leftRearMotorName("lr")
+            .leftFrontMotorName("lf")
+            .leftFrontMotorDirection(DcMotorSimple.Direction.REVERSE)
+            .leftRearMotorDirection(DcMotorSimple.Direction.REVERSE)
+            .rightFrontMotorDirection(DcMotorSimple.Direction.FORWARD)
+            .rightRearMotorDirection(DcMotorSimple.Direction.FORWARD);
+
+    public static DriveEncoderConstants localizerConstants = new DriveEncoderConstants()
+            .rightFrontMotorName("rf")
+            .rightRearMotorName("rr")
+            .leftRearMotorName("lr")
+            .leftFrontMotorName("lf")
+            .leftFrontEncoderDirection(Encoder.FORWARD)
+            .leftRearEncoderDirection(Encoder.FORWARD)
+            .rightFrontEncoderDirection(Encoder.FORWARD)
+            .rightRearEncoderDirection(Encoder.FORWARD)
+            .robotLength(18)
+            .robotWidth(18);
 }
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/Test.java
@@ -0,0 +1,35 @@
+package org.firstinspires.ftc.teamcode.pedroPathing;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Servo;
+
+@TeleOp(name = "Test", group= "Linear OpMode")
+public class Test extends LinearOpMode {
+    DcMotor motor;
+    Servo servo;
+    @Override
+    public void runOpMode(){
+//        motor = hardwareMap.get(DcMotor.class, "test_motor");
+//        servo = hardwareMap.get(Servo.class, "test_servo");
+//        waitForStart();
+//        while(opModeIsActive()) {
+//            if (gamepad1.dpad_down) {
+//                motor.setPower(0.5);
+//                servo.setPosition(0.5);
+//            } else if (gamepad1.dpad_up) {
+//                motor.setPower(-0.5);
+//                servo.setPosition(0);
+//            } else{
+//                motor.setPower(0);
+//            }
+//            // This makes the motor position show up on the screen (driver station)
+//            telemetry.addLine(String.valueOf(motor.getCurrentPosition()));
+//            telemetry.update();
+//        }
+        ForwardTuner tuner = new ForwardTuner();
+        tuner.init_loop();
+        // Multiplier: 1.2769911882186948
+    }
+}
